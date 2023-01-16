24e12f0bb18bbf9056c2d6f9d115c0bf8cdf8646
b19094 <e1b19094@oit.ac.jp>
2023-01-16 22:59:53 +0900

    message

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("HERO!");
+      Hero hero = new Hero();
+      System.out.println(hero.toString());
+    }
+  }
