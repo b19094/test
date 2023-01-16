1f4bf3e05cc7fda3aa85d13a5b3a6ff89b0bcc3c
b19094 <e1b19094@oit.ac.jp>
2023-01-16 23:42:38 +0900

    edit

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
       System.out.println("HERO!");
-      Hero hero = new Hero();
-      System.out.println(hero.toString());
+      Player player = new Player();
+      player.drawHeros();
+      player.showDeck();
     }
   }
